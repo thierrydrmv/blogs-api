e10e3d5791eb3b15df16b31fdefc5c73e757c6ce
Thierry <varelathierry@gmail.com>
2023-02-01 00:00:16 -0300

    feat: model User

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  })
+
+  return User;
+}
