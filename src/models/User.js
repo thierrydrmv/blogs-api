0881b25f502b8a3e9d0b5b8a9a890e7aa2f8562f
Thierry <varelathierry@gmail.com>
2023-02-04 10:32:47 -0300

    model BlogPost

--- src/models/User.js
+++ src/models/User.js
@@ -12,5 +12,10 @@ module.exports = (sequelize, DataTypes) => {
     underscored: true,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'userId', as: 'blogPosts'})
+  }
+
   return User;
 }
